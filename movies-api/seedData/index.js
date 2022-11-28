b9a7694adadf52374a08458a9e10f95d545fb166
Haoxuan Gu <985121841@qq.com>
2022-11-28 17:00:07 +0000

    Mongo lab Exercises done

--- movies-api/seedData/index.js
+++ movies-api/seedData/index.js
@@ -1,5 +1,7 @@
 import userModel from '../api/users/userModel';
+import genreModel from '../api/genres/genreModel';
 import users from './users';
+import genres from './genres';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -15,7 +17,18 @@ async function loadUsers() {
     console.error(`failed to Load user Data: ${err}`);
   }
 }
+async function loadGenres () {
+    console.log('load genres Data');
+    try {
+      await genreModel.deleteMany();
+      await genreModel.collection.insertMany(genres);
+      console.info(`${genres.length} genres were successfully stored.`);
+    } catch (err) {
+      console.error(`failed to Load genres Data: ${err}`);
+    }
+  }
 
 if (process.env.SEED_DB) {
   loadUsers();
+  loadGenres();
 }
