4a98b51378757638036919188cf56e835613db30
Haoxuan Gu <985121841@qq.com>
2022-11-28 16:21:21 +0000

    Exercise Week 9

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
-
+import moviesRouter from './api/movies';
+import genresRouter from './api/genres';
 dotenv.config();
 
 const app = express();
@@ -8,6 +9,11 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.static('public'));
+
+
+app.use('/api/movies', moviesRouter);
+
+app.use('/api/genres', genresRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
