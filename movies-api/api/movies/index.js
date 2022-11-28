e6665fd11505df710b17d7e5f4f89aca54ee18f5
Haoxuan Gu <985121841@qq.com>
2022-11-28 17:14:37 +0000

    Topic 11 - Update Movies Data/Route

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,8 +1,24 @@
 import express from 'express';
 import { movies, movieReviews, movieDetails } from './moviesData';
 import uniqid from 'uniqid'
+import movieModel from './movieModel';
+import asyncHandler from 'express-async-handler';
+
 const router = express.Router(); 
 
+router.get('/', asyncHandler(async (req, res) => {
+    const movies = await movieModel.find();
+    res.status(200).json(movies);
+}));
+router.get('/:id', asyncHandler(async (req, res) => {
+    const id = parseInt(req.params.id);
+    const movie = await movieModel.findByMovieDBId(id);
+    if (movie) {
+        res.status(200).json(movie);
+    } else {
+        res.status(404).json({message: 'The resource you requested could not be found.', status_code: 404});
+    }
+}));
 router.post('/:id/reviews', (req, res) => {
     const id = parseInt(req.params.id);
 
